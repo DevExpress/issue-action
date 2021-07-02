0a099100e7fe8522fe7ca1906c79926e399df7da
Uriah <uriah@devexpress.com>
2021-07-02 12:19:09 +0300

    escape single quotes

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ try {
             }
         ]
     };
-    const msg = JSON.stringify(obj);
+    const msg = JSON.stringify(obj).replace(/[\/\(\)\']/g, '&apos;');
     core.setOutput('json', msg);
 } catch (error) {
     core.setFailed(error.message);
